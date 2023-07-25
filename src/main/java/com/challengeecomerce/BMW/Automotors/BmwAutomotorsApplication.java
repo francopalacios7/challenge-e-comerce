746a5af016c928a9903e4a86cf3fd86b0438321e
Franco Palacios <francopalacios7@yahoo.com.ar>
2023-07-25 11:19:38 -0300

    v1

---
+++ src/main/java/com/challengeecomerce/BMW/Automotors/BmwAutomotorsApplication.java
@@ -0,0 +1,13 @@
+package com.challengeecomerce.BMW.Automotors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmwAutomotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmwAutomotorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challengeecomerce/BMW/Automotors/BmwAutomotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengeecomerce.BMW.Automotors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmwAutomotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
