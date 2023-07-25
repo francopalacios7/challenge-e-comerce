8761b027198e21048050fa803f5b22dbf4c90355
LCMatias98 <correalucasmatias98@gmail.com>
2023-07-25 17:15:06 -0300

    Se crea pagina de inicio sesion

---
+++ src/main/resources/static/javascript/login.js
@@ -0,0 +1,20 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+        //Propiedades Reactivas
+    }
+  },
+  mounted() {
+   
+  },
+
+  created(){
+
+  },
+  methods:{
+
+    
+  }
+}).mount('#app');
