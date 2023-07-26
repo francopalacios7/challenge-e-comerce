86f43b2db32226095095d79fb07f67cf61670163
Root <echanizmauriciogmail.com>
2023-07-26 13:01:14 -0300

    Implementada capa de Spring Security

---
+++ src/main/java/com/challengeecomerce/BMW/Automotors/configurations/WebAuthorization.java
@@ -0,0 +1,79 @@
+package com.mindhub.homebanking.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@EnableWebSecurity
+@Configuration
+class WebAuthorization {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+//                .antMatchers(HttpMethod.POST, ).hasAuthority("CLIENT")
+//                .antMatchers(HttpMethod.POST,  ).hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST,"/api/clients").permitAll()
+//                .antMatchers(HttpMethod.PATCH, "/api/clients/current/cards/{id}", "/api/clients/current/accounts/{id}").hasAuthority("CLIENT")
+                .antMatchers("/api/login", "/web/htmlPages/index.html", "/web/JsPages/**", "/web/stylePages/**", "/web/resources/**", "/web/htmlPages/login.html", "/api/logout").permitAll()
+                .antMatchers("/web/htmlPages/", "/rest/**", "/web/htmlPages/manager.html","/api/clients","/web/htmlPages/adminLoans.html", "/h2-console/**" ).hasAuthority("ADMIN")
+                .antMatchers("/web/htmlPages/**", "/api/clients/current","/api/accounts/**").hasAuthority("CLIENT")
+                .anyRequest().denyAll();
+
+
+
+
+        http.formLogin()
+
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .loginPage("/api/login");
+
+        http.logout().logoutUrl("/api/logout").deleteCookies("JSESSIONID");
+
+        http.cors().disable();
+
+        // turn off checking for CSRF tokens
+        http.csrf().disable();
+
+        //disabling frameOptions so h2-console can be accessed
+        http.headers().frameOptions().disable();
+
+        // if user is not authenticated, just send an authentication failure response
+        http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // if login is successful, just clear the flags asking for authentication
+        http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+
+        // if login fails, just send an authentication failure response
+        http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // if logout is successful, just send a success response
+        http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+
+        return http.build();
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+
+        }
+
+    }
+
+
+}
