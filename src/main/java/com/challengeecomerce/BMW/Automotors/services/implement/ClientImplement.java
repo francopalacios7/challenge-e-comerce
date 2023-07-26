84885c3105516617e6605863bcbdc356b66de985
Franco Palacios <francopalacios7@yahoo.com.ar>
2023-07-26 13:00:21 -0300

    services

---
+++ src/main/java/com/challengeecomerce/BMW/Automotors/services/implement/ClientImplement.java
@@ -0,0 +1,15 @@
+package com.challengeecomerce.BMW.Automotors.services.implement;
+
+import com.challengeecomerce.BMW.Automotors.models.Client;
+import com.challengeecomerce.BMW.Automotors.repositories.ClientRepository;
+import com.challengeecomerce.BMW.Automotors.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ClientImplement implements ClientService{
+    @Autowired
+    ClientRepository clientRepository;
+    @Override
+    public Client findByEmail(String email) {return clientRepository.findByEmail(email);}
+    @Override
+    public void save(Client client) {clientRepository.save(client);}
+}
