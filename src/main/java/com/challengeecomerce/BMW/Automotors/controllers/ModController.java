cd42151eef4bf8910bbfca607ce7bcadf4247b05
MicaGPili <micagpili@gmail.com>
2023-07-30 19:19:34 -0300

    hice dinamicos los enums y lo actualize en el js

--- src/main/java/com/challengeecomerce/BMW/Automotors/controllers/ModController.java
+++ src/main/java/com/challengeecomerce/BMW/Automotors/controllers/ModController.java
@@ -1,10 +1,15 @@
 package com.challengeecomerce.BMW.Automotors.controllers;
 
+import com.challengeecomerce.BMW.Automotors.models.enums.ModType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class ModController {
-
+    @GetMapping("/mods")
+    public ModType[] getAllMods(){
+        return ModType.values();
+    }
 }
