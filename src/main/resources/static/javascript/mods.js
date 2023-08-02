2f899b5ce52b8513565f9693fd9635c79d3bc3cc
Gothman240 <paez.federico96@gmail.com>
2023-08-02 18:56:06 -0300

    aún + + mas estilos

--- src/main/resources/static/javascript/mods.js
+++ src/main/resources/static/javascript/mods.js
@@ -5,6 +5,9 @@ createApp({
     return {
       mods: [],
       arrayCarrito: [],
+      searchText: "", 
+      stockAvailable: false, 
+      selectedColor: "null",
     };
   },
   created() {
@@ -97,6 +100,8 @@ createApp({
 
       console.log(typeof newArray);
     },
+    
+    // ... otros métodos
   },
   computed: {
     funcionPrecioTotal() {
@@ -105,5 +110,38 @@ createApp({
         0
       );
     },
+    
+    filteredMods() {
+      if (
+        this.searchText === "" &&
+        !this.stockAvailable &&
+        this.selectedColor === "null"
+      ) {
+        return this.mods;
+      } else {
+        return this.mods.filter((mod) => {
+          let passesSearchTextFilter =
+            mod.name.toLowerCase().includes(this.searchText.toLowerCase());
+          let passesStockFilter = !this.stockAvailable || mod.stock > 0;
+          let passesColorFilter =
+            this.selectedColor == "null" || mod.carColor == this.selectedColor;
+
+            console.log(typeof mod.carColor)
+            console.log(typeof this.selectedColor)
+
+          return (
+            passesSearchTextFilter &&
+            passesStockFilter &&
+            passesColorFilter
+          );
+        });
+      }
+    },
+  
+
+
+
+
   },
+ 
 }).mount("#app");
