cec0b33e19819b82f7b730495c79d1cf03db7865
MicaGPili <micagpili@gmail.com>
2023-08-03 11:25:13 -0300

    hice el form para pagar un pedido

--- src/main/resources/static/javascript/purchasePayment.js
+++ src/main/resources/static/javascript/purchasePayment.js
@@ -1,3 +1,5 @@
+
+
 const { createApp } = Vue
 
 createApp({
@@ -33,6 +35,45 @@ createApp({
             axios.post("http://localhost:4200/api/pagos")
             .then(res => console.log(res))
             .catch(err => console.log(err))
-        }
+        },
+        confirmOperation() {
+            const swalWithBootstrapButtons = Swal.mixin({
+              customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
+              },
+              buttonsStyling: false
+            })
+      
+            swalWithBootstrapButtons.fire({
+              title: 'Are you sure?',
+              text: "You are about to make a new payment.",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonText: 'Yes, create.',
+              cancelButtonText: 'No, cancel.',
+              reverseButtons: true
+            }).then((result) => {
+              if (result.isConfirmed) {
+                swalWithBootstrapButtons.fire(
+                  'Payment succesfully done!',
+                  'See you soon.',
+                  'success',
+                  console.log("payment created!!!"),
+                  this.cardPayment(),
+      
+                )
+              } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+              ) {
+                swalWithBootstrapButtons.fire(
+                  'Cancelled',
+                  'No payment done :)',
+                  'error'
+                )
+              }
+            })
+          }
     }
 }).mount('#app')
